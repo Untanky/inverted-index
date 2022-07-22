3f4868540df9081cc72efbe949070dc0838c58ea
Lukas Grimm <lukaskingsmail@gmail.com>
2022-07-22 17:22:00 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const helloWorld = () => {
+  console.log('Hello World');
+  console.log('VERY LONG LINE!!!!!!!!!!!!!!!!!!!!!!!!!111111111111111222222211111112121212121213wqqqqqqq111111111');
+};
+
+helloWorld();
