8c2fdd60ce2af7bae30f96f2a51866460fbcd94c
Lukas Grimm <lukaskingsmail@gmail.com>
2022-07-25 21:40:59 +0200

    Misc

--- src/index/handle.ts
+++
@@ -1,5 +0,0 @@
-import { Reduce, Tokenize } from './reduce';
-
-export const handle = <Document, Position>(tokenizer: Tokenize<Document, Position>, ...sanitizer: Reduce<Position>[]) => (document: Document) => {
-  
-}
